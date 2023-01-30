6c89d6afb315d11c1ce02f1b96c0ef3ef5fb1fcb
Cakeageddon <collinvanleeuwen@gmail.com>
2023-01-30 23:12:27 +0100

    bugfix

--- src/main/java/com/example/AutoVault/controllers/SubscriptionController.java
+++ src/main/java/com/example/AutoVault/controllers/SubscriptionController.java
@@ -17,32 +17,32 @@ public class SubscriptionController {
 
     public SubscriptionController(SubscriptionService SubscriptionService) {this.subscriptionService = SubscriptionService;}
 
-    @GetMapping("/Subscription")
+    @GetMapping("/subscription")
     public ResponseEntity<List<SubscriptionDto>> getAllSubscription() {
         return ResponseEntity.ok(subscriptionService.getAllSubscriptions());
     }
 
-    @GetMapping("/Subscription/{id}")
+    @GetMapping("/subscription/{id}")
     public ResponseEntity<SubscriptionDto> getOneSubscription(@PathVariable Long id) {
         SubscriptionDto SubscriptionDto = subscriptionService.getOneSubscription(id);
         return ResponseEntity.ok(SubscriptionDto);
     }
 
-    @PostMapping("/Subscription")
+    @PostMapping("/subscription")
     public ResponseEntity<Object> createSubscription(@RequestBody SubscriptionInputDto subscriptionInputDto) {
         SubscriptionDto subscriptionSavedLocal = subscriptionService.createSubscription(subscriptionInputDto);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(subscriptionSavedLocal.getId()).toUri();
-        return ResponseEntity.created(location).body("Created Subscription with id: " + subscriptionSavedLocal.getId());
+        return ResponseEntity.created(location).body("Created subscription with id: " + subscriptionSavedLocal.getId());
     }
 
-    @PutMapping("/Subscription/{id}")
+    @PutMapping("/subscription/{id}")
     public ResponseEntity<Object> updateSubscription(@PathVariable Long id, @RequestBody SubscriptionInputDto s) {
         return ResponseEntity.ok(subscriptionService.updateSubscription(id, s));
     }
 
-    @DeleteMapping("/Subscription/{id}")
+    @DeleteMapping("/subscription/{id}")
     public ResponseEntity<Object> deleteSubscription(@PathVariable Long id) {
         subscriptionService.deleteSubscription(id);
         return ResponseEntity.ok("Subscription with id " + id + " is deleted.");
